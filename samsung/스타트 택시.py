d1ef5d3efa4f310a63b8443162c8a0b497b9fbae
Eungyu-code <Agyu625@gmail.com>
2023-01-30 16:06:00 +0900

    삼성전자 기출문제

--- samsung/스타트 택시.py
+++ samsung/스타트 택시.py
@@ -1,5 +1,5 @@
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
 N, M, fuel = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
@@ -9,22 +9,22 @@ start, end = [], []
 
 for _ in range(M):
     sx, sy, ex, ey = map(int, input().split())
-    start.append([sx - 1, sy - 1])
-    end.append([ex - 1, ey - 1])
+    start.append([sx, sy])
+    end.append([ex, ey])
 
 
 def move_to_passenger():
     dist = [[-1] * N for _ in range(N)]
     queue = [taxi]
+    dist[taxi[0]][taxi[1]] = 0
     min_dist = int(1e9)
     candidate = []
     while queue:
         x, y = queue.pop(0)
         if dist[x][y] > min_dist:
             break
-        if [x, y] in start:
+        if [x, y] in candidate:
             candidate.append([x, y])
-            min_dist = dist[x][y]
         for k in range(4):
             nx, ny = x + dx[k], y + dy[k]
             if 0 <= nx < N and 0 <= ny < N:
@@ -34,12 +34,13 @@ def move_to_passenger():
     if not candidate:
         return -1, -1, -1
     candidate.sort()
-    return dist[candidate[0][0]][candidate[0][1]], candidate[0][0], candidate[0][1]
+    return candidate[0][0], candidate[0][1], dist[candidate[0][0]][candidate[0][1]]
 
 
 def move_to_destination(end_x, end_y):
     dist = [[-1] * N for _ in range(N)]
     queue = [taxi]
+    dist[taxi[0]][taxi[1]] = 0
     while queue:
         x, y = queue.pop(0)
         if x == end_x and y == end_y:
@@ -54,17 +55,17 @@ def move_to_destination(end_x, end_y):
 
 
 for _ in range(M):
-    start_dist, sx, sy = move_to_passenger()
-    if start_dist == -1 or fuel < start_dist:
+    sx, sy, start_dist = move_to_passenger()
+    if start_dist == -1 or start_dist > fuel:
         fuel = -1
         break
     fuel -= start_dist
     taxi = [sx, sy]
-    idx = start.index([sx, sy])
-    start[idx] = [-1, -1]
-    ex, ey = end[idx]
+    index = start.index([sx, sy])
+    start[index] = [-1, -1]
+    ex, ey = end[index]
     end_dist = move_to_destination(ex, ey)
-    if end_dist == -1 or fuel < end_dist:
+    if end_dist == -1 or end_dist > fuel:
         fuel = -1
         break
     fuel += end_dist
