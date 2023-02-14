5b796bc08c440d5d48512518ec43c1f40ed81bd4
Eungyu-code <Agyu625@gmail.com>
2023-02-14 16:31:46 +0900

    삼성전자 기출문제

--- samsung/어른 상어.py
+++ samsung/어른 상어.py
@@ -2,18 +2,18 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 N, M, k = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-
+arr = [list(map(int, input().split())) for _ in range(N)]
 directions = list(map(int, input().split()))
 priorities = []
-smell = [[[0, 0]] * N for _ in range(N)]
 
-for a in range(M):
+for _ in range(M):
     temp = []
     for _ in range(4):
         data = list(map(int, input().split()))
         temp.append(data)
     priorities.append(temp)
+
+smell = [[[0, 0]] * N for _ in range(N)]
 
 
 def smelling():
@@ -21,37 +21,38 @@ def smelling():
         for j in range(N):
             if smell[i][j][1] > 0:
                 smell[i][j][1] -= 1
-            if graph[i][j] != 0:
-                smell[i][j] = [graph[i][j], k]
+            if arr[i][j] != 0:
+                smell[i][j] = [arr[i][j], k]
 
 
 def solve():
     new_data = [[0] * N for _ in range(N)]
     for i in range(N):
         for j in range(N):
-            if graph[i][j] == 0:
+            if arr[i][j] == 0:
                 continue
-            direction = directions[graph[i][j] - 1]
+            direction = directions[arr[i][j] - 1]
             found = False
-            for index in priorities[graph[i][j] - 1][direction - 1]:
+            for index in priorities[arr[i][j] - 1][direction - 1]:
                 nx, ny = i + dx[index - 1], j + dy[index - 1]
                 if 0 <= nx < N and 0 <= ny < N:
                     if smell[nx][ny][1] == 0:
-                        directions[graph[i][j] - 1] = index
+                        directions[arr[i][j] - 1] = index
                         found = True
                         if new_data[nx][ny] == 0:
-                            new_data[nx][ny] = graph[i][j]
+                            new_data[nx][ny] = arr[i][j]
                         else:
-                            new_data[nx][ny] = min(new_data[nx][ny], graph[i][j])
+                            new_data[nx][ny] = min(new_data[nx][ny], arr[i][j])
                         break
             if found:
                 continue
-            for index in priorities[graph[i][j] - 1][direction - 1]:
+
+            for index in priorities[arr[i][j] - 1][direction - 1]:
                 nx, ny = i + dx[index - 1], j + dy[index - 1]
                 if 0 <= nx < N and 0 <= ny < N:
-                    if smell[nx][ny][0] == graph[i][j]:
-                        directions[graph[i][j] - 1] = index
-                        new_data[nx][ny] = graph[i][j]
+                    if smell[nx][ny][0] == arr[i][j]:
+                        directions[arr[i][j] - 1] = index
+                        new_data[nx][ny] = arr[i][j]
                         break
     return new_data
 
@@ -59,19 +60,17 @@ def solve():
 answer = 0
 while True:
     smelling()
-    graph = solve()
-    answer += 1
-
+    arr = solve()
     check = True
     for a in range(N):
         for b in range(N):
-            if graph[a][b] > 1:
+            if arr[a][b] > 1:
                 check = False
                 break
     if check:
         print(answer)
         break
+    answer += 1
     if answer >= 1000:
         print(-1)
         break
-
