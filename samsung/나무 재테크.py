d6e52a7ff2ae74552fd8c500dc5df9cf33ff3b05
Eungyu-code <Agyu625@gmail.com>
2023-03-26 17:48:31 +0900

    삼성전자 기출문제

--- samsung/나무 재테크.py
+++ samsung/나무 재테크.py
@@ -2,40 +2,41 @@ dx = [-1, -1, -1, 0, 0, 1, 1, 1]
 dy = [-1, 0, 1, -1, 1, -1, 0, 1]
 
 N, M, K = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
 nutrients = [[5] * N for _ in range(N)]
+arr = [list(map(int, input().split())) for _ in range(N)]
 trees = [[[] for _ in range(N)] for _ in range(N)]
-death = [[[] for _ in range(N)] for _ in range(N)]
 
 for _ in range(M):
     x, y, z = map(int, input().split())
     trees[x - 1][y - 1].append(z)
 
 
-def spring():
+def tree_sort():
     for i in range(N):
         for j in range(N):
             if not trees[i][j]:
                 continue
             trees[i][j].sort()
-            length = len(trees[i][j])
-            for _ in range(length):
-                age = trees[i][j].pop(0)
-                if nutrients[i][j] < age:
-                    death[i][j].append(age)
-                else:
-                    nutrients[i][j] -= age
-                    trees[i][j].append(age + 1)
 
 
-def summer():
+def spring_summer():
+    dead_trees = [[0] * N for _ in range(N)]
     for i in range(N):
         for j in range(N):
-            if not death[i][j]:
+            if not trees[i][j]:
                 continue
-            while death[i][j]:
-                age = death[i][j].pop()
-                nutrients[i][j] += age // 2
+            length = len(trees[i][j])
+            for k in range(length):
+                if trees[i][j][k] <= nutrients[i][j]:
+                    nutrients[i][j] -= trees[i][j][k]
+                    trees[i][j][k] += 1
+                else:
+                    for _ in range(k, length):
+                        dead_trees[i][j] += trees[i][j].pop() // 2
+                    break
+    for i in range(N):
+        for j in range(N):
+            nutrients[i][j] += dead_trees[i][j]
 
 
 def fall():
@@ -43,13 +44,12 @@ def fall():
         for j in range(N):
             if not trees[i][j]:
                 continue
-            for age in trees[i][j]:
-                if age % 5 != 0:
-                    continue
-                for k in range(8):
-                    nx, ny = i + dx[k], j + dy[k]
-                    if 0 <= nx < N and 0 <= ny < N:
-                        trees[nx][ny].append(1)
+            for k in range(len(trees[i][j])):
+                if trees[i][j][k] % 5 == 0:
+                    for n in range(8):
+                        nx, ny = i + dx[n], j + dy[n]
+                        if 0 <= nx < N and 0 <= ny < N:
+                            trees[nx][ny].append(1)
 
 
 def winter():
@@ -59,14 +59,14 @@ def winter():
 
 
 for _ in range(K):
-    spring()
-    summer()
+    tree_sort()
+    spring_summer()
     fall()
     winter()
 
-alive = 0
+answer = 0
 for a in range(N):
     for b in range(N):
-        alive += len(trees[a][b])
+        answer += len(trees[a][b])
 
-print(alive)
+print(answer)
--- samsung/미세먼지 안녕.py
+++ samsung/미세먼지 안녕.py
@@ -1,83 +1,80 @@
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
-r, c, t = map(int, input().split())
-graph = []
+R, C, T = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(R)]
 cleaner = []
 
-for a in range(r):
-    data = list(map(int, input().split()))
-    graph.append(data)
-    for b in range(len(data)):
-        if data[b] == -1:
-            cleaner.append([a, b])
+for i in range(R):
+    for j in range(C):
+        if arr[i][j] == -1:
+            cleaner.append([i, j])
 
 
 def diffusion():
-    temp = [[0] * c for _ in range(r)]
-    for i in range(r):
-        for j in range(c):
-            if graph[i][j] == -1:
+    temp = [[0] * C for _ in range(R)]
+    for i in range(R):
+        for j in range(C):
+            if arr[i][j] == 0 or arr[i][j] == -1:
                 continue
-            count = 0
+            now = arr[i][j]
             for k in range(4):
-                nx = i + dx[k]
-                ny = j + dy[k]
-                if 0 <= nx < r and 0 <= ny < c and graph[nx][ny] != -1:
-                    temp[nx][ny] += graph[i][j] // 5
-                    count += 1
-            graph[i][j] -= (graph[i][j] // 5) * count
-    for i in range(r):
-        for j in range(c):
-            graph[i][j] += temp[i][j]
+                nx, ny = i + dx[k], j + dy[k]
+                if 0 <= nx < R and 0 <= ny < C:
+                    if arr[nx][ny] == -1:
+                        continue
+                    arr[i][j] -= now // 5
+                    temp[nx][ny] += now // 5
+
+    for i in range(R):
+        for j in range(C):
+            arr[i][j] += temp[i][j]
 
 
-def upper_clean():
+def upper_cleaner():
+    cx, cy = cleaner[0][0], cleaner[0][1] + 1
     ux = [0, -1, 0, 1]
     uy = [1, 0, -1, 0]
-
-    x, y = cleaner[0][0], cleaner[0][1] + 1
-    res = graph[x][y]
-    graph[x][y] = 0
-    di = 0
+    ud = 0
+    temp = arr[cx][cy]
+    arr[cx][cy] = 0
     while True:
-        nx, ny = x + ux[di], y + uy[di]
-        if 0 > nx or nx >= r or 0 > ny or ny >= c:
-            di += 1
-            continue
-        if graph[nx][ny] == -1:
+        nx, ny = cx + ux[ud], cy + uy[ud]
+        if [nx, ny] == cleaner[0]:
             break
-        res, graph[nx][ny] = graph[nx][ny], res
-        x, y = nx, ny
+        if nx < 0 or nx >= R or ny < 0 or ny >= C:
+            ud += 1
+            continue
+        temp, arr[nx][ny] = arr[nx][ny], temp
+        cx, cy = nx, ny
 
 
-def lower_clean():
+def lower_cleaner():
+    cx, cy = cleaner[1][0], cleaner[1][1] + 1
     lx = [0, 1, 0, -1]
     ly = [1, 0, -1, 0]
-
-    x, y = cleaner[1][0], cleaner[1][1] + 1
-    res = graph[x][y]
-    graph[x][y] = 0
-    di = 0
+    ld = 0
+    temp = arr[cx][cy]
+    arr[cx][cy] = 0
     while True:
-        nx, ny = x + lx[di], y + ly[di]
-        if 0 > nx or nx >= r or 0 > ny or ny >= c:
-            di += 1
-            continue
-        if graph[nx][ny] == -1:
+        nx, ny = cx + lx[ld], cy + ly[ld]
+        if [nx, ny] == cleaner[1]:
             break
-        res, graph[nx][ny] = graph[nx][ny], res
-        x, y = nx, ny
+        if nx < 0 or nx >= R or ny < 0 or ny >= C:
+            ld += 1
+            continue
+        temp, arr[nx][ny] = arr[nx][ny], temp
+        cx, cy = nx, ny
 
 
-for _ in range(t):
+for _ in range(T):
     diffusion()
-    upper_clean()
-    lower_clean()
+    upper_cleaner()
+    lower_cleaner()
 
 answer = 0
-for i in range(r):
-    for j in range(c):
-        answer += graph[i][j]
+for i in range(R):
+    for j in range(C):
+        answer += arr[i][j]
 
 print(answer + 2)
