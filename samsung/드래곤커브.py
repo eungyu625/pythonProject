f76ae1d45bfe7c9a5469dc528be8a233cf49db32
Eungyu-code <Agyu625@gmail.com>
2023-04-22 18:44:46 +0900

    삼성전자 기출문제

--- samsung/드래곤커브.py
+++ samsung/드래곤커브.py
@@ -1,31 +1,27 @@
-dy = [0, -1, 0, 1]
 dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
 
 N = int(input())
-graph = [[0] * 110 for _ in range(110)]
-dragon = []
+arr = [[0] * 110 for _ in range(110)]
 
 for _ in range(N):
-    dragon.append(list(map(int, input().split())))
+    x, y, d, g = map(int, input().split())
+    arr[y][x] = 1
+    curves = [d]
+    for now in range(1, g + 1):
+        length = len(curves)
+        new_curve = []
+        for a in range(length):
+            new_curve.append((curves[a] + 1) % 4)
+        new_curve.reverse()
+        curves = curves + new_curve
+    for curve in curves:
+        x, y = x + dx[curve], y + dy[curve]
+        arr[y][x] = 1
 
-for x, y, d, g in dragon:
-    graph[y][x] = 1
-    curve = [d]
-    for _ in range(g):
-        length = len(curve)
-        for i in range(length - 1, -1, -1):
-            nd = curve[i]
-            curve.append((nd + 1) % 4)
-    nx, ny = x, y
-    for direction in curve:
-        nx += dx[direction]
-        ny += dy[direction]
-        graph[ny][nx] = 1
-
-result = 0
-for i in range(101):
-    for j in range(101):
-        if graph[i][j] == 1 and graph[i + 1][j] == 1 and graph[i][j + 1] == 1 and graph[i + 1][j + 1] == 1:
-            result += 1
-
-print(result)
+answer = 0
+for i in range(105):
+    for j in range(105):
+        if arr[i][j] == 1 and arr[i + 1][j] == 1 and arr[i][j + 1] == 1 and arr[i + 1][j + 1] == 1:
+            answer += 1
+print(answer)
